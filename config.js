4569276f2f6fdd0662d9e497a8792ee60bf19bbc
190n <benjamin.j.grant@gmail.com>
2018-11-26 14:18:28 -0800

    Tighten movement

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ const squirrelMass = 0.3,
     flamethrowerForce = 5,
     flamethrowerAccel = flamethrowerForce / squirrelMass * pixelsToMeter,
     flamethrowerMaxOffsetAngle = Math.PI / 6, // 30°
-    flamethrowerTurnRate = Math.PI / 3; // 60°/sec
+    flamethrowerTurnRate = Math.PI * 2; // 360°/sec
