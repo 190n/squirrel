6b7c242a22bbf4016ddf0e4914cc2157b8e6dd1a
190n <benjamin.j.grant@gmail.com>
2019-02-04 14:06:32 -0800

    Add multiple levels

--- level.js
+++ level.js
@@ -1,21 +1,24 @@
 class Level extends GameObject {
     static preload() {
         Level.sprite = loadImage('level.png');
-        Level.level1 = loadJSON('level1.json');
+        let levelURL = 'level' + (location.hash == '' ? '1' : location.hash.substr(1)) + '.json';
+        Level.levelData = loadJSON(levelURL);
     }
 
     constructor() {
         super(Level.sprite);
-        this.data = Level.level1;
+        this.data = Level.levelData;
     }
 
     draw() {
-        // fill('red');
-        // noStroke();
-        // for (let plat of this.data.platforms) {
-        //     rect(plat.x, plat.y, plat.w, plat.h);
-        // }
-
-        image(Level.sprite, 0, 300);
+        if (this.data.name == 'Level 1') {
+            image(Level.sprite, 0, 300);
+        } else {
+            fill('black');
+            noStroke();
+            for (let plat of this.data.platforms) {
+                rect(plat.x, plat.y, plat.w, plat.h);
+            }
+        }
     }
 }
